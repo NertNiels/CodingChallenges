ba9faae3f1cd82e114ac349534a48df2c88b7ec9
Niels Smit <nertniels@gmail.com>
2018-10-02 17:55:41 +0200

    Added socket.io and node.js

---
+++ Node Server Test/public/sketch.js
@@ -0,0 +1,14 @@
+
+function setup() {
+  createCanvas(600, 400);
+  background(0);
+}
+
+function mouseDragged() {
+  noStroke();
+  fill(255);
+  ellipse(mouseX, mouseY, 20, 20);
+}
+
+function draw() {
+}
---
+++ Node Server Test/server.js
@@ -0,0 +1,17 @@
+
+var express = require("express");
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static("public"));
+
+console.log("Server running...");
+
+var socket = require('socket.io');
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+
+}
