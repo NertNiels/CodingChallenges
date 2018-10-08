8c1b740a07d84360323dd8210fdaa38f5fba0aca
Niels Smit <nertniels@gmail.com>
2018-10-08 17:41:38 +0200

    Created a simple oscillator width notes.

---
+++ Pitch Recognition/sketch.js
@@ -0,0 +1,38 @@
+
+var carrier;
+
+var freq = 261.6256;
+
+var note = "Wait..."
+
+function setup() {
+  createCanvas(1200, 800);
+
+  carrier = new p5.Oscillator('sine');
+  carrier.amp(1.0, 1);
+  carrier.freq(freq);
+  carrier.start();
+}
+
+function keyPressed() {
+  if(keyIsDown(32)) carrier.amp(0, 1);
+  if(keyIsDown(13)) carrier.amp(1.0, 1);
+}
+
+function draw() {
+  background(51);
+
+  textSize(140);
+  fill(255);
+  textAlign(CENTER, CENTER)
+
+  if(random(10) < 0.1) {
+    var i = floor(random(names.length));
+
+    carrier.freq(notes[i]);
+    note = names[i];
+  }
+
+  text(note, width/2, height/3);
+
+}
